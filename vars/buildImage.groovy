287c81f21d69f64f3f4b63228a275abef886520f
Adam Russo <98548957+analystrusso@users.noreply.github.com>
2026-07-24 22:41:13 -0400

    Fix typo in docker login command

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@ def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t analystrusso/twn-bootcamp-repo:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --pasword-stdin'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push analystrusso/twn-bootcamp-repo:jma-2.0'
     }
 }
